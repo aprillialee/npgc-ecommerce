38dbb286d09753a4af3e836535fef3d814e0867f
aprillialee <aprilliapmlee@gmail.com>
2020-04-05 18:40:45 +0100

    added a sign in componet and styling for this

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Switch, Route} from "react-router-dom";
 import HomePage from "./Pages/Homepage/Homepage.Component";
 import ShopPage from "./Pages/Shop/Shop.Component";
 import Header from "./Components/Header/Header.Component";
+import SignInAndSignUpPage from "./Pages/Sign-in-and-sign-up/Sign-in-and-sign-up.Component";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/Pages/Sign-in-and-sign-up/Sign-in-and-sign-up.Component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SignIn from "../../Components/Sign-In/Sign-In.Component";
+
+import "./sign-in-and-sign-up.style.scss";
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
