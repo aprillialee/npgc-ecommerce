163a11857902ab3207babd4dc3c4480f511af8e4
aprillialee <aprilliapmlee@gmail.com>
2020-04-09 22:57:53 +0100

    added a sign-up component

--- src/Pages/Sign-in-and-sign-up/Sign-in-and-sign-up.Component.jsx
+++ src/Pages/Sign-in-and-sign-up/Sign-in-and-sign-up.Component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import SignIn from "../../Components/Sign-In/Sign-In.Component";
+import SignUp from "../../Components/Sign-Up/Sign-Up.Component";
 
 import "./sign-in-and-sign-up.style.scss";
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
