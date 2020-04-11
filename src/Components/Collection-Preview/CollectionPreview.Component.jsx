738d3b80d19b0bd2fb5020bfeec0df60e2e38c65
aprillialee <aprilliapmlee@gmail.com>
2020-04-11 16:56:38 +0100

    shop data updated with unique ids, created a cart utils to group item quantity

--- src/Components/Collection-Preview/CollectionPreview.Component.jsx
+++ src/Components/Collection-Preview/CollectionPreview.Component.jsx
@@ -4,14 +4,14 @@ import CollectionItem from "../Collection-Item/Collection-Item.Component.jsx";
 
 import "./collectionpreview.style.scss";
 
-const CollectionPreview = ({title, items}) => (
+const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map(({id, ...otherItemProps}) => (
-          <CollectionItem key={id} {...otherItemProps} />
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
