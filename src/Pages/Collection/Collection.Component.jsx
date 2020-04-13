d5f0964e5eb2213739fcadeb736abb4d2e8f7ac6
aprillialee <aprilliapmlee@gmail.com>
2020-04-13 15:52:52 +0100

    added collection page routing

---
+++ src/Pages/Collection/Collection.Component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import CollectionItem from "../../Components/Collection-Item/Collection-Item.Component";
+
+import { selectCollection } from "../../redux/shop/shop.selectors";
+
+import "./collection-style.scss";
+
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
+  return (
+    <div className="collection-page">
+      <h2>Category Page</h2>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
